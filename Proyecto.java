6a5a443bf89639e753b14fb5bb1324fd62be5aae
Mateo Rendon MacBook <rendoncode@gmail.com>
2025-03-01 16:35:43 -0600

    creacion del repo

---
+++ Proyecto.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        
+        
+    }
+}
